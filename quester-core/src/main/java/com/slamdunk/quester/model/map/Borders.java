bae2b8110979a6e19c7fcb0c31eafc9d87fbd151
Opack <opack@users.noreply.github.com>
2013-07-08 21:56:04 +0000

    - MOD : Déplacement des tableaux de positions voisines dans une classe à part. - MOD : Un Borders peut désormais renvoyer son côté opposé.

--- quester-core/src/main/java/com/slamdunk/quester/model/map/Borders.java
+++ quester-core/src/main/java/com/slamdunk/quester/model/map/Borders.java
@@ -2,4 +2,18 @@ package com.slamdunk.quester.model.map;
 
 public enum Borders {
 	BOTTOM, LEFT, RIGHT, TOP;
+
+	public Borders getOpposingBorder() {
+		switch (this) {
+		case BOTTOM:
+			return TOP;
+		case LEFT:
+			return RIGHT;
+		case RIGHT:
+			return LEFT;
+		case TOP:
+			return BOTTOM;
+		}
+		return null;
+	}
 };
