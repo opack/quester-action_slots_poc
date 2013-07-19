7517c6a2a74faab11d287959755606e3ba0b5a5b
Opack <opack@users.noreply.github.com>
2013-07-19 18:20:04 +0000

    ADD : Hyper attribut

---
+++ quester-core/src/main/java/com/slamdunk/quester2/puzzle/CrossMatchEffect.java
@@ -0,0 +1,31 @@
+package com.slamdunk.quester2.puzzle;
+
+import com.slamdunk.quester.model.points.Point;
+import com.slamdunk.quester2.puzzle.PuzzleLogic.AttributeData;
+
+/**
+ * Ajoute une quantité d'attribut force aux actions courantes
+ */
+public class CrossMatchEffect extends PuzzleMatchEffect {
+
+	@Override
+	public void perform(PuzzleMatchData matchData) {
+		// Déclenchement des attributs de base
+		for (AttributeData data : matchData.getAttributes()) {
+			eat(data.position, data.attribute, true);
+		}
+		
+		// Chute des attributs restants
+		int count = matchData.count();
+		if (count > 0) {
+			puzzle.fall();
+		}
+		
+		// Ajout d'un petit bonus
+		System.out.println("DBG AttributeAlignmentEffect.perform() ADD BONUS CROSS " + matchData.getSource().attribute);
+
+		// Création d'un hyper
+		Point sourcePos = matchData.getSource().position;
+		puzzle.createAttribute(sourcePos.getX(), sourcePos.getY(), PuzzleAttributes.HYPER);
+	}
+}
